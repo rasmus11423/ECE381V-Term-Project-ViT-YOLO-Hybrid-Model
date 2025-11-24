8328bff82297ed325778cef886cc23df53c75b63
rasmus11423 <s215160@dtu.dk>
2025-11-24 19:00:38 +0200

    time on sh file too much

--- run_ViT_YOLOS_train.sh
+++ run_ViT_YOLOS_train.sh
@@ -5,7 +5,7 @@
 #SBATCH -p gg                                              # Grace Hopper GPU partition
 #SBATCH -N 6
 #SBATCH -n 6
-#SBATCH -t 72:00:00
+#SBATCH -t 70:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user='rl37272@my.utexas.edu'
 
